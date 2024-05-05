30270f7504395030448bf3a76a968aebb88352fd
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-05-05 20:46:09 +0600

    Js condition basic practice one

---
+++ Js basics/js-conditional/comparision.js
@@ -0,0 +1,35 @@
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(10 >= 10); boro othoba soman kina?
+// console.log(15 >= 10);
+// console.log(5 <= 10); soman nah hole o choto so, true
+// console.log(10 <= 10); choto nah hole o soman so, true
+// console.log(10 == 10); true;
+// console.log(10 == 10.1); false;
+
+
+// ------------------------
+var peyaraPrice = 40;
+var applePrice = 250;
+
+// console.log(peyaraPrice> applePrice); false
+// console.log(peyaraPrice < applePrice); true
+
+
+/**
+ * ==
+ * ===
+*/
+
+// console.log(10 == "10"); true "man soman kina"
+// console.log(10 === "10"); false "man ar type dutoi soman kina?"
+
+
+// ------------------
+
+// console.log(10 != "10") 10 ar 10 ki osoman, output false karon man gulo osoman noi soman
+// console.log(10 !== "10") 10 ar 10 ki osoman, ar tader type ki osoman?  output true karon tader man soman hole o type osoman
+
+
---
+++ Js basics/js-conditional/variable.js
@@ -0,0 +1,43 @@
+/**
+ * VARIABLE ==> CONTAINER
+ */
+
+// number
+var sunglassPrice = 80;
+
+// string 
+var subject = "EEE";
+
+//boolean
+var passed = true;
+
+
+/**
+ * NAMING CONVENTION 
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword 
+ * 6. prefer camelCase
+ */
+
+/**
+ * OPERATION
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN ?
+ * 4. null
+ * 5. Number
+ * 6. prefer camelCase
+ */
+
+/**
+ * isNaN means it is not a number.
+ * If the argument passed in the function is not
+ * a number then it will show true, or if 
+ * the argument passed in the function is a number
+ * then it will show false
+ */
+console.log(isNaN(NaN)) // True, because it is not a number
+console.log(isNaN(4)) // False, because it is a number
