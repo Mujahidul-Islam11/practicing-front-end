26a637b029927dd2a7fdfd6a26c58ebb6f076841
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-05-06 12:44:43 +0600

    Looooooooooooooooop

--- Js-basics/js/for.js
+++ Js-basics/js/for.js
@@ -12,6 +12,16 @@
 */
 
 
-for(let num = 0; num < 5; num++){
-    console.log("Love you priyo");
-}
+// for(let num = 0; num < 5; num++){
+//     console.log(num);
+// }
+
+// even
+// for(let i = 0; i <=10; i=i+2){
+// console.log(i);
+// }
+
+// odd
+// for(let i = 1; i<=10; i=i+2){
+//     console.log(i)
+// }
