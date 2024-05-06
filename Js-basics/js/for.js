9927fd9586be058238afdd150762c9ad4655d8cc
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-05-06 12:16:22 +0600

    Looooooooooooooooooooooooop

---
+++ Js-basics/js/for.js
@@ -0,0 +1,17 @@
+/**
+ * for (  ;  ;  ){
+ * 
+ * }
+ * 
+ * for(first part; second, third){
+ * }
+ * 
+ * 
+ * for(loop variable; condition; loop process){
+ * }
+*/
+
+
+for(let num = 0; num < 5; num++){
+    console.log("Love you priyo");
+}
