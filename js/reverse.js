357df9454a1f0888e24f30806460c08e57e8e40a
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-05-14 21:07:27 +0600

    Reverse a string

---
+++ js/reverse.js
@@ -0,0 +1,25 @@
+const sentence = `I am learning Web Development`;
+
+// for(const letter of sentence){
+    //     reverse = letter + reverse;
+    // }
+    
+    // console.log(sentence);
+    
+    // for(let i = 0; i <sentence.length; i++){
+        //     const letter = sentence[i];
+        //     reverse = letter + reverse;
+        // }
+        // console.log(reverse);
+        
+        // let reverse = sentence.split(); [ 'I am learning Web Development' ]
+        // let reverse = sentence.split(""); [
+        //     'I', ' ', 'a', 'm', ' ', 'l',
+        //     'e', 'a', 'r', 'n', 'i', 'n',
+        //     'g', ' ', 'W', 'e', 'b', ' ',
+        //     'D', 'e', 'v', 'e', 'l', 'o',
+        //     'p', 'm', 'e', 'n', 't'
+        //   ]
+        // let reverse = sentence.split("").reverse().join(''); tnempoleveD beW gninrael ma I 
+        // let reverse = sentence.split("").reverse().join(); t,n,e,m,p,o,l,e,v,e,D, ,b,e,W, ,g,n,i,n,r,a,e,l, ,m,a, ,I 
+        console.log(reverse);
