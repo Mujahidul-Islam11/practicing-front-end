0f048642d3e97de6e466c30f729a1840154e8ed1
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-05-14 22:32:11 +0600

    Pera pera pera with parameter

---
+++ js/func.js
@@ -0,0 +1,16 @@
+// const number = 34;
+
+// function fanOffKor(){
+//     console.log('Ey fan off kor')
+//     console.log('Ey fan off kor')
+//     console.log('Ey fan off kor')
+// }
+
+// fanOffKor()
+
+function peraDisNah(a, b){
+    console.log(a * b); 
+}
+
+peraDisNah(1,6);
+
