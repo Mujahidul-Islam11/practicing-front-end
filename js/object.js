5d75279936738bb378c2ec125d6ef118c6d766ca
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-05-14 21:14:13 +0600

    declear object

---
+++ js/object.js
@@ -0,0 +1,17 @@
+// const age = 21;
+// const school = 'rabm';
+// const isPassed = true;
+// let isDeveloper;
+// const subjects = ['bangla', 'english', 'math', 'physics']
+
+
+
+// const user = {
+//     name: 'Dinar',
+//     age: 23,
+//     isStudent: true,
+//     isDeveloper: true,
+//     hobbies: ['dancing', 'singing', 'painting']
+// }
+
+// console.log(user)
