30081acd1a16ea0868d586aa6db5a424a6a7ff27
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-05-14 22:02:08 +0600

    Nested object

---
+++ js/nested.js
@@ -0,0 +1,14 @@
+const clg = {
+    name: 'vnc',
+    class: ['11', '12'],
+    events: ['science fair', 'bijoy dibos', '21 feb'],
+    unique : {
+        color: "blue",
+        result: {
+            gpa: '5',
+            merit: 'top'
+        }
+    }
+}
+clg.unique.result.merit = ' top top top'
+console.log(clg.unique.result.merit)
