158cf811c492fa6c62064883a6a1563b0c237ac1
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-05-14 20:45:10 +0600

    LowerCase UpperCase mamar biyer sathe sathe Trim mamar biye o khailam

---
+++ js/case.js
@@ -0,0 +1,28 @@
+const school = `RAJ UK Uttara Model School`;
+
+// console.log(school);
+// console.log(school.toLowerCase());
+// console.log(school.toUpperCase());
+
+
+// const subject = 'Chemistry';
+// const book = 'chemistry';
+
+// if(subject.toLowerCase() === book.toLowerCase(){
+//     console.log(`Boi paiya gechi`);
+// }
+// else{
+//     console.log('Poralekhar maire salam');
+// }
+
+
+// const drinks = 'water';
+// const liquid = 'water ';
+
+// if(drinks.trim() === liquid.trim()){
+//     console.log(`Pani khaiya pet fhulamu`);
+// }
+// else{
+//     console.log('Lobon pani khawa jabe nah');
+// } trim() removes white spaces from a whole string
+
