671a450df40990cdb16d2dd599092cf1a0bfaa08
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-05-15 21:47:40 +0600

    ForEach Map Filter method complete

---
+++ js/forEach.js
@@ -0,0 +1,15 @@
+const numbers = [12, 34, 54, 65, 76];
+
+// for(let i = 0; i<numbers.length; i++){
+//     console.log(numbers[i])
+// }
+
+
+// numbers.forEach(myFunc  = (x) =>{
+//     console.log(x)
+//     });
+
+// numbers.map(myFunc  = (x) =>{
+//     return console.log(x)
+//     }); map returns an array
+
