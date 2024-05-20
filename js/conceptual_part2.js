233e6fc3f09ca0928361b6815e5158c6be857e04
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-05-20 15:29:16 +0600

    js to string method

--- js/conceptual_part2.js
+++ js/conceptual_part2.js
@@ -1,5 +1,10 @@
-const id = '7';
+const id = "7";
 // padding
+const fullDigit = id.padStart(6, "0"); //make a six digit number, if there is less
+//number then six digit then add zero before the main digit.
+ // console.log(fullDigit);Output:- 000007
 
-const fullDigit = id.padStart(6, "0");
-console.log(fullDigit) // Output:- 000007
+const studentNumber = 1;
+const studentNumberString = studentNumber.toString(); // convert the number into a string
+
+console.log(studentNumberString)
