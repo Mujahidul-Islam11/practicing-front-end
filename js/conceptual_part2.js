a7973312095644aa3281971f31934321cede1f07
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-05-23 23:49:12 +0600

    Padstart, tostring, substring method recaped

--- js/conceptual_part2.js
+++ js/conceptual_part2.js
@@ -7,7 +7,7 @@ const fullDigit = id.padStart(6, "0"); //make a six digit number, if there is le
 const studentNumber = 1;
 const studentNumberString = studentNumber.toString(); // convert the number into a string
 
-// console.log(studentNumberString.padStart(4, "0"))
+// console.log(studentNumberString.padStart(4, "0")); giving padding before the number, using padStart
 
 const orderOne = 1;
 const firstOrder = orderOne.toString().padStart(4, "0");
