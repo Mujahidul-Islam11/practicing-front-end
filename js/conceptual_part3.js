534ae9f887c6975ee64688b28ae6490c334c8a4c
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-05-24 12:10:24 +0600

    for loop, forEach loop complete

--- js/conceptual_part3.js
+++ js/conceptual_part3.js
@@ -6,4 +6,14 @@ for (let i = 0; i <= 10; i++) {
   sum++;
 }
 
-console.log(sum);
+// console.log(sum);
+
+const names = ["mir", "mezba", "rakib"];
+
+// for(let i = 0; i <3; i++){
+//     console.log(names[i]);
+// }
+
+// names.forEach(name => {
+//     console.log(name);
+// });
