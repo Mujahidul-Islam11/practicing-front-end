f714ddfe619f8464cccfb4e5f7e2142755ee5030
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-05-25 21:01:34 +0600

    Find, filter method

--- js/conceptual_part3.js
+++ js/conceptual_part3.js
@@ -21,17 +21,17 @@ const names = ["mir", "mezba", "rakib"];
 //     console.log(name);
 // });
 
-const mentor ={
-  name: "ravi",
-  age: 24,
-  isDeveloper: true
-}
+// const mentor ={
+//   name: "ravi",
+//   age: 24,
+//   isDeveloper: true
+// }
 
-const keys = Object.keys(mentor);
+// const keys = Object.keys(mentor);
 // console.log(keys)
-for(let i = 0; i < keys.length; i++){
-  console.log(mentor[keys[i]]);
-}
+// for(let i = 0; i < keys.length; i++){
+//   console.log(mentor[keys[i]]);
+// }
 
 // how to access object values
 /**
@@ -41,3 +41,46 @@ for(let i = 0; i < keys.length; i++){
 // for(let key in mentor){
 //   console.log(mentor[key])
 // }
+
+
+// callback 
+
+// const randomFunc = (fn) =>{
+//   return fn(3);
+// }
+
+// randomFunc((number)=>{
+//   console.log(number)
+// })
+
+const mentorList = [
+  {
+    name: "ravi",
+    age: 25,
+    isDeveloper: true
+  },
+  {
+    name: "ravi",
+    age: 28,
+    isDeveloper: true
+  },
+  {
+    name: "ravi",
+    age: 26,
+    isDeveloper: true
+  }
+]
+
+// const mentor25 = mentorList.filter(mentor => {
+//   if(mentor.age > 25)
+//   return mentor
+// })
+// filter er kaj jegula jegula match kore ogula dea
+// console.log(mentor25)
+
+// const mentor25 = mentorList.find(mentor => {
+//   if(mentor.age === 25)
+//   return mentor
+// })
+// find er kaj specific ekti single data khuje dea
+// console.log(mentor25)
