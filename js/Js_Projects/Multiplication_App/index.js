80e9245f3cfdaebe8c93b99deb904569dbf79e00
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-05-28 15:39:53 +0600

    multiplication app finished

--- js/Js_Projects/Multiplication_App/index.js
+++ js/Js_Projects/Multiplication_App/index.js
@@ -1,4 +1,28 @@
-let num1 = Math.floor(Math.random() * 10);
-let num2 = Math.floor(Math.random() * 10);
+let num1 = Math.ceil(Math.random() * 10);
+let num2 = Math.ceil(Math.random() * 10);
 
-console.log(num1)
+const questionEl = document.getElementById("question");
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score");
+
+questionEl.innerHTML = `What is ${num1} multiply by ${num2}?`;
+
+const correctAns = num1 * num2;
+let score = localStorage.getItem("score");
+scoreEl.innerHTML = `Score:- ${score}`
+
+formEl.addEventListener("submit", () => {
+  const userAns = +inputEl.value;
+  if (userAns === correctAns) {
+    score++;
+    updateLocalStorage()
+  } else {
+    score--;
+    updateLocalStorage()
+  }
+});
+
+const updateLocalStorage = ()=>{
+    localStorage.setItem("score", JSON.stringify(score))
+}
