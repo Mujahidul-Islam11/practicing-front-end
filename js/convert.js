3dbc42feef529dd6ab73e6eef9d2747abf1a07c3
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-11-18 16:38:44 +0600

    Problem - remove duplicate items from array solved

--- js/convert.js
+++ js/convert.js
@@ -28,10 +28,45 @@ function gramToKiloGram(gram) {
 
 function leapYear(year) {
   if (year % 4 === 0) {
-    console.log(year + " is a leap year")
-  }else{
-    console.log(year + " isn't leap year")
+    console.log(year + " is a leap year");
+  } else {
+    console.log(year + " isn't leap year");
   }
 }
 
-leapYear(2024);
+function leapYear2(year) {
+  if (year % 100 !== 0 && year % 4 === 0) {
+    console.log(year + " is a leap year");
+  } else {
+    console.log(year + " isn't leap year");
+  }
+}
+
+// leapYear2(2024);
+
+/**
+ * 1. Those years which are not divisible by 100, but divisible by 4 are leap year
+ */
+
+const numbers = [1, 24, 56, 45, 12, 53, 88];
+
+// numbers.forEach((num) => {
+//   if (num % 2 === 0) {
+//     console.log(num + " is a even number");
+//   } else if (num % 2 === 1) {
+//     console.log(num + " is a odd number");
+//   }
+// });
+
+const ppl = ["abul", "abul", "bulbul", "culbul", "bulbul", "culbul pande"];
+
+const newPpl = [];
+ppl.forEach((item) => {
+  if (newPpl.includes(item)) {
+    return;
+  } else {
+    newPpl.push(item);
+  }
+});
+
+console.log(ppl, ppl.length, newPpl, newPpl.length);
