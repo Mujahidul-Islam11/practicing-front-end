cf2f190a6d0873a0f29a590974352eab408fd2a0
Mohammad Dinar <web.dev.dinar@gmail.com>
2025-02-28 18:20:16 +0600

    Async await leason completed

--- Deep_Dive_Js/async_await.js
+++ Deep_Dive_Js/async_await.js
@@ -1,1 +1,44 @@
 // ------------------------- Basic Syntax -----------------
+// async function fetchData() {
+//   console.log("fetching data");
+//   const data = await new Promise((res) => {
+//     setTimeout(() => {
+//       res("data received");
+//     }, 3000);
+//   });
+//   console.log(data);
+// }
+
+// fetchData();
+
+// ----------------------------- Fetch API's -----------------------
+const fetchUsers = async () => {
+  try {
+    const result = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await result.json();
+    console.log(users);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// fetchUsers();
+
+
+const createUser = async()=>{
+    const newUser = {
+        name: "Mohammad Dinar",
+        age: 20
+    }
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newUser)
+    })
+
+    const data = await response.json();
+    console.log(data)
+}
+
+// createUser();
